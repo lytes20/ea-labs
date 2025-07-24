14adc83e88bd4f2cec024bfb940f1c6f41a69bf5
Gideon Bamuleseyo <gideonbamuleseyo@gmail.com>
2025-07-24 01:50:29 -0500

    Remove unused packages

--- Lab03/Lesson3SpringBootJDBCCustomerWithCreditCard/src/main/java/customers/Application.java
+++ Lab03/Lesson3SpringBootJDBCCustomerWithCreditCard/src/main/java/customers/Application.java
@@ -5,8 +5,6 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Random;
-
 @SpringBootApplication
 public class Application implements CommandLineRunner {
 
